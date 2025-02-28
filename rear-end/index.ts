283b33b95d840486adc481657fc90998c62ca07c
Nick <nick-ls@users.noreply.github.com>
2025-02-28 02:36:12 -0800

    push some changes

---
+++ rear-end/index.ts
@@ -0,0 +1,42 @@
+import { delay } from "../communism/lib/delay";
+import { SERVER_GAME_TICK } from "../communism/constants";
+import { Game } from "./Game";
+
+const game = new Game();
+
+let ticks = 0;
+let totalDelta = 0;
+
+//what actually runs the game loop
+(async () => {
+	while (true) {
+		//check time at beginning of gamestep
+		let startTimeCheck = Date.now();
+
+		// update game state
+		game.updateGameState();
+
+		// send updated state to all clients
+		game.broadcastState();
+		// wait until end of tick
+		// broadcast(wss, )
+
+		//check time at end of gamestep
+		let endTimeCheck = Date.now();
+
+		let delta = endTimeCheck - startTimeCheck;
+		ticks++;
+		totalDelta += delta;
+		if (ticks >= 2000) {
+			game.logTicks(ticks, totalDelta);
+			ticks = 0;
+			totalDelta = 0;
+		}
+		//wait until the rest of the tick is complete
+		if (delta > SERVER_GAME_TICK) {
+			//shit we had a longass tick. Cry ig
+		} else {
+			await delay(SERVER_GAME_TICK - delta);
+		}
+	}
+})();
