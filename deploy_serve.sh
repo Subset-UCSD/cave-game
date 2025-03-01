bf9574217067cf90403ebc8cc331a1ecd20511fc
Nick <nick-ls@users.noreply.github.com>
2025-03-01 02:29:09 -0800

    fix deploy

--- deploy_serve.sh
+++ deploy_serve.sh
@@ -2,6 +2,11 @@
 
 # called by .github/workflows/fuckelonmusk.yml
 
+git pull
+
 npm install
 npm run build
-systemctl restart cavegame
+# its like this for a reason, if you change the command format,
+# the visudo rule will fail and it will prompt you for a password
+# to restart cavegame
+sudo /bin/systemctl restart cavegame.service
