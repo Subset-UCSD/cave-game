d1a8751dd4c3cadbe2529951c1223b9ba2590d08
Nick <nick-ls@users.noreply.github.com>
2025-03-01 01:26:14 -0800

    try new deploy method

--- deploy_serve.sh
+++ deploy_serve.sh
@@ -4,22 +4,4 @@
 
 npm install
 npm run build
-
-if [ -f ../cavegame_pid.txt ]; then
-  PID=$(cat ../cavegame_pid.txt)
-  echo try asking it to die
-  if ! kill $PID 2>/dev/null; then
-    echo it did not kill successfully for some reason, give it a second
-    sleep 1
-    echo then really kill it
-    kill -9 $PID || true
-  fi
-fi
-
-echo "[restarted]" >> ../cavegame.log
-date >> ../cavegame.log
-
-# start the server and set it free
-CAVE_GAME_PORT=4269 nohup node dist/index.js >> ../cavegame.log 2>&1 &
-echo $! > ../cavegame_pid.txt
-echo PID=$(cat ../cavegame_pid.txt) >> ../cavegame.log
+systemctl restart cavegame
