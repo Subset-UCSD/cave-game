78fe0e55336dbb79e77836bdc40e1d7be54666ce
Thomas A. Powell <tpowell@pint.com>
2025-06-24 11:22:31 +0000

    hey bestie. ur code fuking SUCKS

--- server/Game.ts
+++ server/Game.ts
@@ -98,7 +98,7 @@ export class Game implements ServerHandlers<ClientMessage, ServerMessage> {
 		return Object.values(entities).sort((a, b) => a.distance - b.distance);
 	}
 
-	getPlayerByEntityId = (id: EntityId) => this.players.values().find(p => p.id === id);
+	getPlayerByEntityId = (id: EntityId) => this.players.values().find((p) => p.id === id);
 
 	private createPlayerEntity(playerNum: number, pos: Vector3 | undefined): PlayerEntity {
 		console.log(playerNum);
@@ -250,7 +250,7 @@ export class Game implements ServerHandlers<ClientMessage, ServerMessage> {
 				camera: Array.from(
 					cameraTransform([0, 20, 20], { y: 0, x: -Math.PI / 8 /* * (Math.sin(Date.now() / 847) + 1)*/, z: 0 }),
 				),
-				
+
 				// cameraInterpolation: {duration:SERVER_GAME_TICK},
 				// physicsBodies: player.debug ? this.world.serialize() : undefined,
 				/*others: Array.from(this.players.values(), (p) =>
